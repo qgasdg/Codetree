f121b7786fc6d28ce102cb713ca725e7a293bb23
qgasdg <96183715+qgasdg@users.noreply.github.com>
2025-05-10 01:31:57 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 233ms, Memory: 26MB, Status: Passed - Codetree

--- 250510/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 250510/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -1,24 +1,22 @@
 K, N = map(int, input().split())
 
 # Please write your code here.
-st = []
-def f(n): # 0부터 시작
-    if n == N:
-        for i in range(N):
-            print(st[i], end=' ')
-        print()
-        return
-    if n < 2:
-        for i in range(1, K + 1):
-            st.append(i)
-            f(n + 1)
-            st.pop()
-    else:
-        for i in range(1, K + 1):
-            if st[-1] == i and st[-2] == i:
-                continue
-            st.append(i)
-            f(n + 1)
-            st.pop()
+permutations = []
 
-f(0)
+def print_per():
+    for i in range(N):
+        print(permutations[i], end=' ')
+    print()
+
+def find_permutations(n):
+    if n == N:
+        print_per()
+        return
+    for i in range(1, K + 1):
+        if n >= 2 and permutations[-1] == i and permutations[-2] == i:
+            continue
+        permutations.append(i)
+        find_permutations(n + 1)
+        permutations.pop()
+
+find_permutations(0)
